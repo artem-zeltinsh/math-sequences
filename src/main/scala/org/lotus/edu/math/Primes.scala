4ef5c78f142ec8e6e91dadddb9628062137da654
Artem Zeltinsh <artem.zeltinsh@gmail.com>
2015-04-07 19:12:53 +0200

    Implementation in Scala

---
+++ src/main/scala/org/lotus/edu/math/Primes.scala
@@ -0,0 +1,25 @@
+package org.lotus.edu.math
+
+/**
+ * Singleton object to deal with prime numbers.
+ */
+object Primes {
+
+  /**
+   * Returns sequence of primes less or equal to the provided boundary.
+   * @param boundary test boundary
+   * @return sequence of primes
+   */
+  def getPrimes(boundary: Int): IndexedSeq[Int] = 2.to(boundary).filter(n => isPrime(n))
+
+
+  /**
+   * Tests if provided number is prime,
+   * @param number number to test
+   * @return true if the number is prime, false otherwise
+   */
+  def isPrime(number: Int): Boolean = {
+    if (number <= 1) return false
+    (2 to math.sqrt(number).toInt).forall(i => number % i != 0)
+  }
+}
